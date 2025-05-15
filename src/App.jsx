95ff27d673781da9f79d8737f5fb9afb6b6dfb87
Thomas Abas <abasienyenethomas2@gmail.com>
2025-05-15 06:34:26 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import  './index.css'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className=' text-3xl bg-blue-600 hover:bg-red-600  text-white px-8 py-4 '>Hello World</div>
+      <form
+        autoComplete="off"
+        className="w-full max-w-[600px] p-10 bg-white rounded-lg shadow"
+        aria-label="signup-form"
+      >
+        <h2 className="mb-10 text-3xl font-bold text-center">Sign Up Form</h2>
+        <div className="flex flex-col items-start mb-5 gap-y-3">
+          <label htmlFor="email" className="text-sm font-medium cursor-pointer">
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            className="w-full p-4 bg-transparent border border-gray-200 rounded-lg outline-none"
+            placeholder="Enter your email address..."
+          />
+        </div>
+        <div className="flex flex-col items-start mb-5 gap-y-3">
+          <label
+            htmlFor="password"
+            className="text-sm font-medium cursor-pointer"
+          >
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            className="w-full p-4 bg-transparent border border-gray-200 rounded-lg outline-none"
+            placeholder="Enter your password"
+          />
+        </div>
+        <div className="flex items-center justify-end mb-5 text-slate-400">
+          <p>Already have an account?</p>
+          <a href="#" className="text-blue-500 underline">
+            Sign In
+          </a>
+        </div>
+        <button
+          type="submit"
+          className=" w-full items-center justify-center px-8 py-4 font-sans font-semibold tracking-wide text-white bg-blue-500 rounded-lg h-[60px]"
+        >
+          Create an account
+        </button>
+      </form>
+      
+     </>
+  )
+}
+
+export default App
