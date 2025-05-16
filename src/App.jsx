6ec0d3c7eb5dfcb472e4f6606ff3753a40c7bf60
Thomas Abas <abasienyenethomas2@gmail.com>
2025-05-16 04:54:57 -0800

    working on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import  './index.css'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   
 
   return (
     <>
-      <div className=' text-3xl bg-blue-600 hover:bg-red-600  text-white p-12 mt-12 '>Hello World</div>
+      <Navbar/>
         
      </>
   )
